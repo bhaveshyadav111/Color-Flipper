8ce6f8d27a3353209dd8009cfeb032fa9e873061
bhavesh yadav <ybhavesh540@gmail.com>
2025-08-05 19:06:01 +0530

    color Flip on tap

---
+++ code.js
@@ -0,0 +1,42 @@
+import { allColors,darkColors,lightColors} from "./color.js"
+
+console.log('hello')
+let Btn = document.querySelector(".Btn")
+let color = document.querySelector(".color")
+let body = document.querySelector("body")
+let dark = document.querySelector(".darkBtn")
+let light = document.querySelector(".lightBtn")
+let reset = document.querySelector(".resetBtn")
+
+const mathRandom = (n)=>{
+    return Math.floor(Math.random()*n);
+}
+// For Random COlors
+
+Btn.addEventListener("click",()=>{
+    let n = allColors.length 
+    let ranNum = (mathRandom(n))
+    body.style.backgroundColor = allColors[ranNum];
+    color.textContent = allColors[ranNum];
+})
+
+// for dark cOlors
+
+dark.addEventListener("click",()=>{
+    let n = darkColors.length
+    let ranNum  = (mathRandom(n))
+    body.style.backgroundColor = darkColors[ranNum]
+    color.textContent = darkColors[ranNum];
+})
+light.addEventListener("click",()=>{
+    let n = lightColors.length
+    let ranNum  = (mathRandom(n))
+    body.style.backgroundColor = lightColors[ranNum]
+    body.style.color = "black"
+    color.textContent = lightColors[ranNum];
+})
+
+reset.addEventListener("click",()=>{
+    body.style.backgroundColor = "#1f1f1f"
+    if(color.textContent != "something coming soon !") color.textContent = "something coming soon !";
+})
